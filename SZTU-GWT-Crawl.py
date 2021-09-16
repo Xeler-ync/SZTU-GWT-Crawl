a0c867c8532eb7bb6bfe45a91b06287b508347a1
Xeler-ync <Xeler-ync@qq.com>
2021-09-17 00:08:44 +0800

    complete email-send, GWT-zhuye-crawl

---
+++ SZTU-GWT-Crawl.py
@@ -0,0 +1,88 @@
+#coding=utf-8
+
+import smtplib
+import os
+import re
+import requests
+import json
+from email.mime.text import MIMEText
+from email.header import Header
+headers={
+    'Referer' : 'http://nbw.sztu.edu.cn/',
+    'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36'
+}
+
+
+def sentSTMPMessage(content):
+    jsonSendingData=openInfosFile()
+    #set sever
+    emailSender=smtplib.SMTP_SSL(jsonSendingData["smtpserver"],jsonSendingData["smtpport"])
+    emailSender.login(jsonSendingData["fromaddress"],jsonSendingData["qqcode"])
+    message = MIMEText(content, 'plain', 'utf-8')   #define content
+    message['From'] = Header(jsonSendingData["fromname"], 'utf-8')   #sender
+    message['Subject'] = Header(jsonSendingData["title"], 'utf-8')  #email title
+    for i in range(len(jsonSendingData["toname"])):
+        message['To'] = Header(jsonSendingData["toname"][i], 'utf-8')   #recever
+        try:
+            emailSender.sendmail(jsonSendingData["fromaddress"], jsonSendingData["toaddress"][i], message.as_string())
+        except Exception as error:
+            print ('Email sent failed --' + str(error))
+        print ('Email sent successfully to '+jsonSendingData["toname"][i]+' '+jsonSendingData["toaddress"][i])
+
+def openInfosFile():#if no, creat one
+    try:
+        with open(file=os.getcwd()+'/sender.email.acc.pss.json',mode='r',encoding='utf-8') as sea:
+            jsonData=json.load(sea)
+            return jsonData
+    except OSError:
+        with open(file=os.getcwd()+'/sender.email.acc.pss.json',mode='w+',encoding='utf-8') as sea:
+            sea.writelines('{')
+            sea.writelines('    "fromaddress": "",\n')
+            sea.writelines('    "fromname": "",\n')
+            sea.writelines('    "toaddress": [\n')
+            sea.writelines('        ""\n')
+            sea.writelines('    ],\n')
+            sea.writelines('    "toname": [\n')
+            sea.writelines('        ""\n')
+            sea.writelines('    ],\n')
+            sea.writelines('    "qqcode": "",\n')
+            sea.writelines('    "smtpserver": "smtp.qq.com",\n')
+            sea.writelines('    "smtpport": 465\n')
+            sea.writelines('    "title": ""\n')
+            sea.writelines('}')
+
+def getGWTPageHTML(page,totalPage):
+    if page==1 and totalPage==0:
+        html=requests.get(url='http://nbw.sztu.edu.cn/list.jsp?urltype=tree.TreeTempUrl&wbtreeid=1029',headers=headers).content.decode('utf-8')
+    else:
+        url='http://nbw.sztu.edu.cn/list.jsp?totalpage='+str(totalPage)+'&PAGENUM='+str(page)+'&urltype=tree.TreeTempUrl&wbtreeid=1029'
+        html=requests.get(url=url,headers=headers).content.decode('utf-8')
+    #http://nbw.sztu.edu.cn/list.jsp?totalpage=236&PAGENUM=2&urltype=tree.TreeTempUrl&wbtreeid=1029
+    return html
+
+def getTotalPageFromFirstPageHTML(html):
+    if html=='':
+        html=getGWTPageHTML()
+    totalPageFinder='上页</span><span class="p_no_d">1</span><span class="p_no"><a href="\?totalpage=([0-9]+)\&PAGENUM=2\&urltype=tree.TreeTempUrl\&wbtreeid=[0-9]+"'
+    totalPage=re.findall(totalPageFinder,html,re.S)
+    return totalPage[0]
+
+def getGWTPageInfo(page,html,totalPage):
+    if page==0:
+        return [['No Page 0']*5]
+    if html=='':
+        html=getGWTPageHTML()
+    if totalPage==0:
+        totalPage=getTotalPageFromFirstPageHTML(html)
+    html=html.replace('\n','')
+    html=html.replace('\r','')
+    if page>int(totalPage):
+        return [['Page too big, max is '+str(totalPage)]*5]
+    listFinder='style="font-size: 14px;">(.*?)</a></div><div class="pull-left width04 txt-elise text-left" style="width:54%;"><a href="info/[0-9]+/([0-9]+).htm" title=".*?" target="_blank" style="">(.*?)</a></div><div class="pull-left width05"  style="width:5%;height:32px;">(.*?)</div><div class="pull-right width06"  style="width:11%;">([0-9-]+)</div></li>'
+    announcementInfoList=re.findall(listFinder,html,re.S)#source, index, title, hasAttachment, date
+    return announcementInfoList, totalPage
+
+# sentSTMPMessage('Fuck content to my fucking mail box again.')
+
+# (announcementInfoList,totalPage)=getGWTPageInfo(1,'',0)
+# print(announcementInfoList[0])
