9291751280fa22b51cc3962776d0acb53e713593
Xeler-ync <Xeler-ync@qq.com>
2021-11-12 15:26:46 +0800

    去除对没用到的jsonpath的import

--- SZTU-GWT-Crawl.py
+++ SZTU-GWT-Crawl.py
@@ -5,7 +5,6 @@ import os
 import re
 import requests
 import json
-import jsonpath
 from lxml import etree
 import time
 import datetime
@@ -15,6 +14,8 @@ from email.mime.application import MIMEApplication
 from email.header import Header
 from email.utils import formataddr
 from email.header import Header
+
+
 headers={
     'Referer' : 'http://nbw.sztu.edu.cn/',
     'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36'
