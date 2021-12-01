ad9bc9b6e49a544d4a07e955ffbb76f0382ea0fe
Xeler-ync <Xeler-ync@qq.com>
2021-12-01 16:39:27 +0800

    代码拆分

---
+++ FunctionSentEmail.py
@@ -0,0 +1,78 @@
+#coding=utf-8
+
+from TimeLibs import *
+from SentEmailLibs import *
+
+from FunctionFileControl import openInfosFile
+
+
+def create_email_content_from_new_announcement(AnnouncementInfo,start_time):
+    content = 'There is(are) '+str(len(AnnouncementInfo.academy_list))+' new announcement(s) now!\n'
+    content += 'The message was generated at '+start_time+'\n'
+    content += '\n'
+    for i in range(len(AnnouncementInfo.academy_list)):
+        content += '\n'
+        content += 'From: '+AnnouncementInfo.academy_list[i]+'\n'
+        content += 'Date: '+AnnouncementInfo.date_list[i]+'\n'
+        content += 'Tittle: '+AnnouncementInfo.title_list[i]+'\n'
+        content += 'Link: '+'http://nbw.sztu.edu.cn/info/'+AnnouncementInfo.index_list[i]+'.htm\n'
+        if AnnouncementInfo.has_attachment_list[i].find('1') == -1:
+            content += 'Attachment: Flase\n'
+        else:
+            content += 'Attachment: True\n'
+    content += '\n'
+    content += '\n'
+    content += 'The message was sent at '+'datetime.datetime.now().strftime("%y-%m-%d_%H:%M:%S")'+'\n'
+    content += '\n'
+    content += '本程序所提供的信息，仅供参考之用。所有数据来自深圳技术大学内部网，版权归深圳技术大学及相关发布人所有。\n'
+    content += '完整的免责声明见程序发布页或向邮件发送者索取'
+    return content
+
+# def separate_new_announcement(AnnouncementInfo.attachment_file_list):
+    # new_announcement = []
+    # for i in range(len(AnnouncementInfo.attachment_file_list)):
+    #     if AnnouncementInfo.attachment_file_list[i][3].find('r') == -1:
+    #         new_announcement.append(AnnouncementInfo.attachment_file_list[i])
+    # return new_announcement
+
+def mark_sent_announcement(AnnouncementInfo) -> None:
+    with open(file='./GWT.previous.cache.txt',mode='r',encoding='utf-8') as gpc:
+        previous_code_list = gpc.readlines()
+        for i in range(len(AnnouncementInfo.index_list)):
+            for j in range(len(previous_code_list)):
+                if (AnnouncementInfo.index_list[i]+'\n') == previous_code_list[j]:
+                    AnnouncementInfo.index_list[i] += 'r'
+    return None
+
+def send_GWT_message(content,AnnouncementInfo): # ,all_HTML_name,an1nouncement_info_list):
+    json_sending_data = openInfosFile()
+    #set sever
+    email_sever = smtplib.SMTP_SSL(json_sending_data["smtpserver"],json_sending_data["smtpport"])
+    email_sever.login(json_sending_data["fromaddress"],json_sending_data["qqcode"])
+    message = MIMEMultipart()   #define message
+    message['from'] = formataddr([json_sending_data["fromname"],json_sending_data["fromaddress"]])#sender
+    if len(AnnouncementInfo.academy_list) == 0:
+        message['Subject'] = Header('No new GWT announcement', 'utf-8')  #email title
+    else:
+        message['Subject'] = Header(json_sending_data["title"], 'utf-8')  #email title
+    for i in range(len(AnnouncementInfo.HTML_file_list)):
+        HTMLFile = MIMEApplication(open('./html-download/{}.htm'.format(AnnouncementInfo.HTML_file_list[i]),mode='r',encoding='utf-8').read())
+        HTMLFile.add_header('Content-Disposition', 'attachment', filename=AnnouncementInfo.HTML_file_list[i]+'.htm')
+        message.attach(HTMLFile)
+    for i in range(len(AnnouncementInfo.attachment_file_list)):
+        attachmentFile = MIMEApplication(open('./html-download/'+AnnouncementInfo.attachment_file_list[i],'rb').read())
+        attachmentFile.add_header('Content-Disposition', 'attachment', filename=AnnouncementInfo.attachment_file_list[i])
+        message.attach(attachmentFile)
+    content = content.replace('datetime.datetime.now().strftime("%y-%m-%d_%H:%M:%S")',datetime.datetime.now().strftime("%y-%m-%d_%H:%M:%S"))
+    message.attach(MIMEText(content, 'plain', 'utf-8'))
+    for i in range(len(json_sending_data["to"])):
+        if len(AnnouncementInfo.academy_list) == 0 and not json_sending_data['to'][i]["isadmin"]:
+            print('No new announcement, ignore '+json_sending_data["to"][i]["name"]+' '+json_sending_data["to"][i]["address"])
+            continue
+        message["To"] = formataddr([json_sending_data["to"][i]["name"],json_sending_data["to"][i]["address"]])#recever
+        try:
+            email_sever.sendmail(from_addr = json_sending_data["fromaddress"], to_addrs=[json_sending_data["to"][i]["address"]], msg=message.as_string())
+            print('Email sent successfully to '+json_sending_data["to"][i]['name']+' '+json_sending_data["to"][i]["address"])
+        except Exception as error:
+            print('Email sent failed --> ' + str(error))
+    email_sever.quit()
--- SZTU-GWT-Crawl.py
+++ SZTU-GWT-Crawl.py
@@ -1,86 +1,15 @@
 #coding=utf-8
 
 from TimeLibs import *
-from SentEmailLibs import *
 from CrawlLibs import *
 
 from FunctionFileControl import *
 from FunctionCrawlPage import *
+from FunctionSentEmail import *
 
 from ClassHeaders import *
 from ClassSubPageInfo import *
 
-
-def create_email_content_from_new_announcement():
-    content = 'There is(are) '+str(len(AnnouncementInfo.academy_list))+' new announcement(s) now!\n'
-    content += 'The message was generated at '+start_time+'\n'
-    content += '\n'
-    for i in range(len(AnnouncementInfo.academy_list)):
-        content += '\n'
-        content += 'From: '+AnnouncementInfo.academy_list[i]+'\n'
-        content += 'Date: '+AnnouncementInfo.date_list[i]+'\n'
-        content += 'Tittle: '+AnnouncementInfo.title_list[i]+'\n'
-        content += 'Link: '+'http://nbw.sztu.edu.cn/info/'+AnnouncementInfo.index_list[i]+'.htm\n'
-        if AnnouncementInfo.has_attachment_list[i].find('1') == -1:
-            content += 'Attachment: Flase\n'
-        else:
-            content += 'Attachment: True\n'
-    content += '\n'
-    content += '\n'
-    content += 'The message was sent at '+'datetime.datetime.now().strftime("%y-%m-%d_%H:%M:%S")'+'\n'
-    content += '\n'
-    content += '本程序所提供的信息，仅供参考之用。所有数据来自深圳技术大学内部网，版权归深圳技术大学及相关发布人所有。\n'
-    content += '完整的免责声明见程序发布页或向邮件发送者索取'
-    return content
-
-# def separate_new_announcement(AnnouncementInfo.attachment_file_list):
-    # new_announcement = []
-    # for i in range(len(AnnouncementInfo.attachment_file_list)):
-    #     if AnnouncementInfo.attachment_file_list[i][3].find('r') == -1:
-    #         new_announcement.append(AnnouncementInfo.attachment_file_list[i])
-    # return new_announcement
-
-def mark_sent_announcement() -> None:
-    with open(file=os.getcwd()+'/GWT.previous.cache.txt',mode='r',encoding='utf-8') as gpc:
-        previous_code_list = gpc.readlines()
-        for i in range(len(AnnouncementInfo.index_list)):
-            for j in range(len(previous_code_list)):
-                if (AnnouncementInfo.index_list[i]+'\n') == previous_code_list[j]:
-                    AnnouncementInfo.index_list[i] += 'r'
-    return None
-
-def send_GWT_message(content): # ,all_HTML_name,an1nouncement_info_list):
-    json_sending_data = openInfosFile()
-    #set sever
-    email_sever = smtplib.SMTP_SSL(json_sending_data["smtpserver"],json_sending_data["smtpport"])
-    email_sever.login(json_sending_data["fromaddress"],json_sending_data["qqcode"])
-    message = MIMEMultipart()   #define message
-    message['from'] = formataddr([json_sending_data["fromname"],json_sending_data["fromaddress"]])#sender
-    if len(AnnouncementInfo.academy_list) == 0:
-        message['Subject'] = Header('No new GWT announcement', 'utf-8')  #email title
-    else:
-        message['Subject'] = Header(json_sending_data["title"], 'utf-8')  #email title
-    for i in range(len(AnnouncementInfo.HTML_file_list)):
-        HTMLFile = MIMEApplication(open(os.getcwd()+'/html-download/{}.htm'.format(AnnouncementInfo.HTML_file_list[i]),mode='r',encoding='utf-8').read())
-        HTMLFile.add_header('Content-Disposition', 'attachment', filename=AnnouncementInfo.HTML_file_list[i]+'.htm')
-        message.attach(HTMLFile)
-    for i in range(len(AnnouncementInfo.attachment_file_list)):
-        attachmentFile = MIMEApplication(open(os.getcwd()+'/html-download/'+AnnouncementInfo.attachment_file_list[i],'rb').read())
-        attachmentFile.add_header('Content-Disposition', 'attachment', filename=AnnouncementInfo.attachment_file_list[i])
-        message.attach(attachmentFile)
-    content = content.replace('datetime.datetime.now().strftime("%y-%m-%d_%H:%M:%S")',datetime.datetime.now().strftime("%y-%m-%d_%H:%M:%S"))
-    message.attach(MIMEText(content, 'plain', 'utf-8'))
-    for i in range(len(json_sending_data["to"])):
-        if len(AnnouncementInfo.academy_list) == 0 and not json_sending_data['to'][i]["isadmin"]:
-            print('No new announcement, ignore '+json_sending_data["to"][i]["name"]+' '+json_sending_data["to"][i]["address"])
-            continue
-        message["To"] = formataddr([json_sending_data["to"][i]["name"],json_sending_data["to"][i]["address"]])#recever
-        try:
-            email_sever.sendmail(from_addr = json_sending_data["fromaddress"], to_addrs=[json_sending_data["to"][i]["address"]], msg=message.as_string())
-            print('Email sent successfully to '+json_sending_data["to"][i]['name']+' '+json_sending_data["to"][i]["address"])
-        except Exception as error:
-            print('Email sent failed --> ' + str(error))
-    email_sever.quit()
 
 def get_total_page_from_first_page_HTML(html):
     if html == '':
@@ -130,13 +59,13 @@ if __name__ == '__main__':
         start_time = datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
         Headers = HeadersMoudle(cookie='')
         (AnnouncementInfo,total_page) = get_GWT_page_info(1,'',0)
-        mark_sent_announcement()
+        mark_sent_announcement(AnnouncementInfo)
         save_recent_GWT_code(AnnouncementInfo.index_list)
         AnnouncementInfo.remove_duplicate_page()
         # new_announcement_list = separate_new_announcement(AnnouncementInfo.attachment_file_list)
-        emailContent = create_email_content_from_new_announcement()
+        emailContent = create_email_content_from_new_announcement(AnnouncementInfo,start_time)
         print(str(len(AnnouncementInfo.academy_list))+' new announcement(s)')
         download_web_file(AnnouncementInfo,Headers.headers)
-        send_GWT_message(emailContent) # ,AnnouncementInfo.HTML_file_list,AnnouncementInfo.attachment_file_list
+        send_GWT_message(emailContent,AnnouncementInfo) # ,AnnouncementInfo.HTML_file_list,AnnouncementInfo.attachment_file_list
         print('Sleep from '+datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')+' to '+(datetime.datetime.now()+datetime.timedelta(hours=pause_hours)).strftime('%Y-%m-%d_%H:%M:%S'))
         time.sleep(pause_hours*3600)
